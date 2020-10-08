2ab8b90ec1bf21e552d55047593b0cb3c7133415
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-10-08 08:46:25 +0200

    Formatted code

--- src/App.js
+++ src/App.js
@@ -9,56 +9,56 @@ Sass.setWorkerUrl("sass.worker.js")
 
 const sass = new Sass()
 sass.options({
-  style: Sass.style.compressed
+    style: Sass.style.compressed
 })
 
 // Object.values(scssCode).forEach((library) => {
-//   Object.keys(library.imports).forEach((fileName, code) => {
-//     sass.writeFile(fileName, code)
-//   })
+//     Object.keys(library.imports).forEach((fileName, code) => {
+//         sass.writeFile(fileName, code)
+//     })
 // })
 
 sass.writeFile("./subs/general.scss", `body {
-    background-color: orange;
-    color: blue;
+        background-color: orange;
+        color: blue;
 }`)
 
 function App() {
-  const [result, setResult] = useState(null)
-  const scssInput = `@import "./subs/general";`
+    const [result, setResult] = useState(null)
+    const scssInput = `@import "./subs/general";`
 
-  useEffect(() => {
-    if (result === null) {
-      compile()
+    useEffect(() => {
+        if (result === null) {
+            compile()
+        }
+    }, [result])
+
+    const compile = () => {
+        sass.compile(scssInput, result => {
+            console.log(result)
+            setResult(result)
+        })
     }
-  }, [result])
 
-  const compile = () => {
-    sass.compile(scssInput, result => {
-      console.log(result)
-      setResult(result)
-    })
-  }
+    return (
+        <div className="App">
+            <style
+                type="text/css"
+                dangerouslySetInnerHTML={{
+                    __html: `${result && result.text ? result.text : ""}`
+                }}
+            />
 
-  return (
-    <div className="App">
-      <style
-        type="text/css"
-        dangerouslySetInnerHTML={{
-          __html: `${result && result.text ? result.text : ""}`
-        }}
-      />
+            {(result && result.message) && 
+                <div className="alert alert-danger">
+                    {result.message}
+                </div>
+            }
 
-      {(result && result.message) && 
-        <div className="alert alert-danger">
-          {result.message}
+            {scssInput}
+            <button onClick={compile}>Compile</button>
         </div>
-      }
-
-      {scssInput}
-      <button onClick={compile}>Compile</button>
-    </div>
-  )
+    )
 }
 
 export default App
--- src/scssCode.js
+++ src/scssCode.js
@@ -1,12 +1,12 @@
-/* This code is generated, don't change it manually! */
+/* This code is generated, don"t change it manually! */
 export const scssCode = {
-    "own-lib": {
-        imports: {
-            "./subs/general.scss": `body {
-    background-color: orange;
-    color: blue;
+        "own-lib": {
+                imports: {
+                        "./subs/general.scss": `body {
+        background-color: orange;
+        color: blue;
 }`
-        },
-        main: `@import "./subs/general";`
-    }
+                },
+                main: `@import "./subs/general";`
+        }
 }
