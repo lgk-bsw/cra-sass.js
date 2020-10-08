37a210e398cd0fe4b7df5301284bb669e66fa543
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-10-08 08:43:50 +0200

    created own example lib

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
@@ -12,16 +12,50 @@ sass.options({
   style: Sass.style.compressed
 })
 
+// Object.values(scssCode).forEach((library) => {
+//   Object.keys(library.imports).forEach((fileName, code) => {
+//     sass.writeFile(fileName, code)
+//   })
+// })
+
+sass.writeFile("./subs/general.scss", `body {
+    background-color: orange;
+    color: blue;
+}`)
+
 function App() {
+  const [result, setResult] = useState(null)
+  const scssInput = `@import "./subs/general";`
+
+  useEffect(() => {
+    if (result === null) {
+      compile()
+    }
+  }, [result])
+
   const compile = () => {
-    sass.compile("$blue: blue; body { background: $blue; }", result => {
+    sass.compile(scssInput, result => {
       console.log(result)
+      setResult(result)
     })
   }
 
   return (
     <div className="App">
-      {scssCode["blue-react"]}
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `${result && result.text ? result.text : ""}`
+        }}
+      />
+
+      {(result && result.message) && 
+        <div className="alert alert-danger">
+          {result.message}
+        </div>
+      }
+
+      {scssInput}
       <button onClick={compile}>Compile</button>
     </div>
   )
